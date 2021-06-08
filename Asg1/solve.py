85018af6e22fff17c0261670ce36e07ea17e7b8d
Ze Sheng <z7sheng@uwaterloo.ca>
2021-06-08 15:11:54 -0400

    Degenerated DFS

--- Asg1/solve.py
+++ Asg1/solve.py
@@ -46,6 +46,52 @@ def a_star(init_board, hfn):
     # raise NotImplementedError
 
 
+
+# def dfs(init_board):
+#     """
+#     Run the DFS algorithm given an initial board.
+
+#     If the function finds a goal state, it returns a list of states representing
+#     the path from the initial state to the goal state in order and the cost of
+#     the solution found.
+#     Otherwise, it returns am empty list and -1.
+
+#     :param init_board: The initial board.
+#     :type init_board: Board
+#     :return: (the path to goal state, solution cost)
+#     :rtype: List[State], int
+#     """
+#     def __index_gcar(cars):
+#         return next(i for i, car in enumerate(cars) if car.is_goal == True)
+    
+    
+#     goalCoord = init_board.size - 2     # pre-defined
+
+#     origin = State(init_board, zero_heuristic, 0, 0)
+#     if is_goal(origin):
+#         return [origin], 0
+    
+#     st, mem = [origin], set()
+#     while st:
+#         # ### debug
+#         # print(len(st), st[-1].id)
+#         # st[-1].board.display()
+
+#         cur = st.pop()
+#         if cur.id not in mem:
+#             mem.add(cur.id)
+#             if pre_goal(cur):   # tail pruning
+#                 return (get_path(cur) + 
+#                         [gen_secondary_state(cur, __index_gcar(cur.board.cars), goalCoord)], 
+#                         cur.depth + 1)
+#             else:
+#                 st += get_successors(cur)
+    
+#     return [], -1
+#     # raise NotImplementedError
+
+
+
 def dfs(init_board):
     """
     Run the DFS algorithm given an initial board.
@@ -60,34 +106,21 @@ def dfs(init_board):
     :return: (the path to goal state, solution cost)
     :rtype: List[State], int
     """
-    def __index_gcar(cars):
-        return next(i for i, car in enumerate(cars) if car.is_goal == True)
-    
-    
-    goalCoord = init_board.size - 2     # pre-defined
-
     origin = State(init_board, zero_heuristic, 0, 0)
-    if is_goal(origin):
-        return [origin], 0
     
     st, mem = [origin], set()
     while st:
-        # ### debug
-        # print(len(st), st[-1].id)
-        # st[-1].board.display()
-
         cur = st.pop()
         if cur.id not in mem:
             mem.add(cur.id)
-            if pre_goal(cur):   # tail pruning
-                return (get_path(cur) + 
-                        [gen_secondary_state(cur, __index_gcar(cur.board.cars), goalCoord)], 
-                        cur.depth + 1)
+            if is_goal(cur):
+                return get_path(cur), cur.depth
             else:
                 st += get_successors(cur)
     
     return [], -1
     # raise NotImplementedError
+
 
 
 def gen_secondary_state(state, carInd, secCoord):
