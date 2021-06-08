4b69f8db6e1150e82993165739641da07ba42e8b
Ze Sheng <z7sheng@uwaterloo.ca>
2021-06-08 02:43:59 -0400

    Asg1 DFS

---
+++ src/main.py
@@ -0,0 +1,53 @@
+import sys
+import unittest
+
+from board import *
+from solve import *
+
+
+def main():
+    # boards = from_file("/home/z7sheng/CS686/cs686-repo/src/tst_case01.txt")
+    # boards = from_file("/home/z7sheng/CS686/cs686-repo/src/tst_case02.txt")
+    # boards = from_file("/home/z7sheng/CS686/cs686-repo/src/tst_case03.txt")
+    # boards = from_file("/home/z7sheng/CS686/cs686-repo/src/tst_case43.txt")
+    # boards = from_file("/home/z7sheng/CS686/cs686-repo/src/tst_case05.txt")
+    # boards = from_file("/home/z7sheng/CS686/cs686-repo/src/tst_case06.txt")
+    # boards = from_file("/home/z7sheng/CS686/cs686-repo/src/tst_case07.txt")
+    boards = from_file("/home/z7sheng/CS686/cs686-repo/src/jams_posted.txt")
+    for k, b in enumerate(boards):
+        # print(b.name)
+        # print(b.grid)
+        # b.display()
+
+        pathDfs, costDfs = dfs(b)
+        print(b.name, costDfs)
+        # print(len(pathDfs))
+        # for i, state in enumerate(pathDfs):
+        #     print(k, i, "/", len(pathDfs))
+        #     state.board.display()
+
+        # retAstar = a_star(b)
+
+
+
+class MyTest(unittest.TestCase):
+    def test_upper(self):
+        """Test the upper() function of class string"""
+        self.assertEqual('foo'.upper(), 'FOO')
+
+    def test_isupper(self):
+        """Test isupper() function of class string"""
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('foo'.isupper())
+        self.assertFalse('foo'.isupper())
+        self.assertFalse('Foo'.isupper())
+
+    def test_failing(self):
+        """A test that fails"""
+        self.assertEqual(True, False)
+
+
+
+if __name__ == '__main__':
+    main()
+    # unittest.main()
---
+++ src/solve.py
@@ -0,0 +1,176 @@
+from board import *
+from copy import deepcopy as cp
+from collections import deque
+
+
+def a_star(init_board, hfn):
+    """
+    Run the A_star search algorithm given an initial board and a heuristic function.
+
+    If the function finds a goal state, it returns a list of states representing
+    the path from the initial state to the goal state in order and the cost of
+    the solution found.
+    Otherwise, it returns am empty list and -1.
+
+    :param init_board: The initial starting board.
+    :type init_board: Board
+    :param hfn: The heuristic function.
+    :type hfn: Heuristic
+    :return: (the path to goal state, solution cost)
+    :rtype: List[State], int
+    """
+
+    raise NotImplementedError
+
+
+def dfs(init_board):
+    """
+    Run the DFS algorithm given an initial board.
+
+    If the function finds a goal state, it returns a list of states representing
+    the path from the initial state to the goal state in order and the cost of
+    the solution found.
+    Otherwise, it returns am empty list and -1.
+
+    :param init_board: The initial board.
+    :type init_board: Board
+    :return: (the path to goal state, solution cost)
+    :rtype: List[State], int
+    """
+    origin = State(init_board, zero_heuristic, 0, 0)
+    st, mem = [origin], set()
+    while st:
+        # ### debug
+        # print(len(st), st[-1].id)
+        # st[-1].board.display()
+
+        cur = st.pop()
+        if cur.id not in mem:
+            mem.add(cur.id)
+            if is_goal(cur):
+                return get_path(cur), cur.depth
+            else:
+                for suc in get_successors(cur):
+                    st.append(suc)
+    
+    return [], -1
+    # raise NotImplementedError
+
+
+def get_successors(state):
+    """
+    Return a list containing the successor states of the given state.
+    The states in the list may be in any arbitrary order.
+
+    :param state: The current state.
+    :type state: State
+    :return: The list of successor states.
+    :rtype: List[State]
+    """
+    ret = []
+    for j, car in enumerate(state.board.cars):
+
+        route = [False] * car.var_coord + [True] * car.length + \
+                [False] * (6 - car.var_coord - car.length)
+        
+        oFlag = car.orientation == 'h'
+        for i in range(6):
+            if (oFlag and state.board.grid[car.fix_coord][i] == '.' or 
+                not oFlag and state.board.grid[i][car.fix_coord] == '.'):
+                route[i] = True
+
+        for i in range(7 - car.length):
+            section = route[min(i, car.var_coord):max(i, car.var_coord) + car.length]
+            if car.var_coord != i and sum(section) == len(section):
+                secondaryCars = cp(state.board.cars)
+                secondaryCars[j].set_coord(i)
+
+                suc = State(Board(state.board.name, state.board.size, secondaryCars), 
+                            state.hfn, state.f, state.depth + 1)
+                
+                suc.parent = state
+                # f, hfn to be edited
+                ret.append(suc)
+
+                # ### debug
+                # print("A")
+                # print(suc.board.cars[j])
+                # print(suc.board.cars[j].orientation, suc.board.cars[j].length)
+                # print(suc.board.cars[j].var_coord, suc.board.cars[j].fix_coord)
+                # print(i, section, route)
+                # state.board.display()
+                # print("B")
+                # suc.board.display()
+
+    return ret
+
+    # raise NotImplementedError
+
+
+def is_goal(state):
+    """
+    Returns True if the state is the goal state and False otherwise.
+
+    :param state: the current state.
+    :type state: State
+    :return: True or False
+    :rtype: bool
+    """
+    for ch in state.board.grid[2][::-1]:
+        if ch == '>':
+            return True
+        elif ch != '.':
+            return False
+
+    # raise NotImplementedError
+
+
+def get_path(state):
+    """
+    Return a list of states containing the nodes on the path 
+    from the initial state to the given state in order.
+
+    :param state: The current state.
+    :type state: State
+    :return: The path.
+    :rtype: List[State]
+    """
+    ret, cur = deque(), state
+    while cur:
+        ret.appendleft(cur)
+        cur = cur.parent
+    
+    return list(ret)
+
+    # raise NotImplementedError
+
+
+def blocking_heuristic(board):
+    """
+    Returns the heuristic value for the given board
+    based on the Blocking Heuristic function.
+
+    Blocking heuristic returns zero at any goal board,
+    and returns one plus the number of cars directly
+    blocking the goal car in all other states.
+
+    :param board: The current board.
+    :type board: Board
+    :return: The heuristic value.
+    :rtype: int
+    """
+
+    raise NotImplementedError
+
+
+def advanced_heuristic(board):
+    """
+    An advanced heuristic of your own choosing and invention.
+
+    :param board: The current board.
+    :type board: Board
+    :return: The heuristic value.
+    :rtype: int
+    """
+
+    raise NotImplementedError
