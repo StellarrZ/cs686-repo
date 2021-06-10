eec60e35d0afc24488ddd1a81b19af91c07778fa
Ze Sheng <z7sheng@uwaterloo.ca>
2021-06-10 13:16:41 -0400

    Final Version

--- Asg1/solve.py
+++ Asg1/solve.py
@@ -22,16 +22,8 @@ def a_star(init_board, hfn):
     """
     origin = State(init_board, hfn, hfn(init_board), 0)
 
-    # # debug
-    # print(hash(init_board))
-    # print(tuple(map(tuple, init_board.grid)))
-
     hp, mem = [(origin.f, origin.id, 0, origin)], set()
     while hp:
-        # # debug
-        # print(len(hp))
-        # print(nsmallest(min(3, len(hp)), hp))
-
         _, curId, _, cur = heappop(hp)
         if curId not in mem:
             mem.add(curId)
@@ -42,8 +34,6 @@ def a_star(init_board, hfn):
                     heappush(hp, (suc.f, suc.id, curId, suc))
     
     return [], -1
-
-    # raise NotImplementedError
 
 
 
@@ -73,10 +63,6 @@ def dfs(init_board):
     
     st, mem = [origin], set()
     while st:
-        # ### debug
-        # print(len(st), st[-1].id)
-        # st[-1].board.display()
-
         cur = st.pop()
         if cur.id not in mem:
             mem.add(cur.id)
@@ -88,7 +74,6 @@ def dfs(init_board):
                 st += sorted(get_successors(cur), key=lambda x: -x.id)
     
     return [], -1
-    # raise NotImplementedError
 
 
 
@@ -120,6 +105,7 @@ def gen_secondary_state(state, carInd, secCoord):
     return suc
 
 
+
 def get_successors(state):
     """
     Return a list containing the successor states of the given state.
@@ -145,22 +131,10 @@ def get_successors(state):
         for i in range(7 - car.length):
             section = route[min(i, car.var_coord):max(i, car.var_coord) + car.length]
             if car.var_coord != i and sum(section) == len(section):
-                # f, hfn to be edited
                 ret.append(gen_secondary_state(state, j, i))
-
-                # ### debug
-                # print("A")
-                # print(suc.board.cars[j])
-                # print(suc.board.cars[j].orientation, suc.board.cars[j].length)
-                # print(suc.board.cars[j].var_coord, suc.board.cars[j].fix_coord)
-                # print(i, section, route)
-                # state.board.display()
-                # print("B")
-                # suc.board.display()
 
     return ret
 
-    # raise NotImplementedError
 
 
 def pre_goal(state):
@@ -181,6 +155,7 @@ def pre_goal(state):
             return False
 
 
+
 def is_goal(state):
     """
     Returns True if the state is the goal state and False otherwise.
@@ -192,7 +167,6 @@ def is_goal(state):
     """
     return True if state.board.grid[2][-1] == '>' else False
 
-    # raise NotImplementedError
 
 
 def get_path(state):
@@ -212,7 +186,6 @@ def get_path(state):
     
     return list(ret)
 
-    # raise NotImplementedError
 
 
 def blocking_heuristic(board):
@@ -232,7 +205,6 @@ def blocking_heuristic(board):
     rightEnd = board.grid[2].index('>') + 1
     return 0 if rightEnd == board.size else 7 - rightEnd - board.grid[2][rightEnd:].count('.')
 
-    # raise NotImplementedError
 
 
 def advanced_heuristic(board):
@@ -270,19 +242,6 @@ def advanced_heuristic(board):
         for x in proj:
             col[x] = False
         return col
-
-    
-    # class PROJ:
-    #     def __init__(self):
-    #         self.proj = set((2))
-        
-    #     def set_proj(self, col, low, high):
-    #         for x in range(low, high + 1):
-    #             if col[x]:
-    #                 self.proj.add(x)
-        
-    #     def to_set(self):
-    #         return self.proj
 
 
     rightEnd = board.grid[2].index('>') + 1
@@ -331,59 +290,3 @@ def advanced_heuristic(board):
     
     ret += len(proj)
     return ret
-
-            # if obstacle == '|':
-            #     ret += 3 - col[4:].count('.')
-            # elif obstacle == '^':
-            #     if board.grid[3][j] == 'v':
-            #         ret += min(3 - col[:2].count('.'), 2 - (col[4][j] == '.'))
-            #     ret += min(3 - col[:2].count('.'), 2 - min(1, col[3:].count('.')))
-            #     pass
-            # else:
-            #     pass
-
-    # ret = 1
-
-    # for j, ch in enumerate(board.grid[2][rightEnd:], rightEnd):
-    #     if ch != '.':
-    #         cnt = 0
-    #         for i in range(6):
-    #             if board.grid[i][j] == '.':
-    #                 cnt += 1
-
-    #         if ch == '|':
-    #             ret += 3 - min(2, cnt)
-    #         else:
-    #             ret += 2 - min(1, cnt)
-    
-    # return ret
-
-
-
-# def coincident_heuristic(board):
-#     """
-#     An advanced heuristic of your own choosing and invention.
-
-#     :param board: The current board.
-#     :type board: Board
-#     :return: The heuristic value.
-#     :rtype: int
-#     """
-#     rightEnd = board.grid[2].index('>') + 1
-#     if rightEnd == board.size:
-#         return 0
-
-#     ret = 1
-#     for j, ch in enumerate(board.grid[2][rightEnd:], rightEnd):
-#         if ch != '.':
-#             cnt = 0
-#             for i in range(6):
-#                 if board.grid[i][j] == '.':
-#                     cnt += 1
-
-#             if ch == '|':
-#                 ret += 3 - min(2, cnt)
-#             else:
-#                 ret += 2 - min(1, cnt)
-    
-#     return ret
